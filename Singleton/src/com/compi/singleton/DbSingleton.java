dbccfce494bfedb05643494089ee5c0c2fdb4f43
C0mpy <n.zunic@yahoo.com>
2018-03-25 19:36:54 +0200

    make Singleton Lazy Load

--- Singleton/src/com/compi/singleton/DbSingleton.java
+++ Singleton/src/com/compi/singleton/DbSingleton.java
@@ -2,11 +2,19 @@ package com.compi.singleton;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
 
-    private DbSingleton() {}
+    private DbSingleton() {
+        if(instance != null) {
+            throw new RuntimeException("Use DbSingleton.getInstance() to create.");
+        }
+    }
 
     public static DbSingleton getInstance() {
+        if(instance == null) {
+            instance = new DbSingleton();
+        }
+
         return instance;
     }
 }
