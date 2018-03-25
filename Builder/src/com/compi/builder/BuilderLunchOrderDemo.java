541f24eaa5e2bd3bf81c656bcdd9c3866ce4a72b
C0mpy <n.zunic@yahoo.com>
2018-03-25 21:01:18 +0200

    Builder Implementation

---
+++ Builder/src/com/compi/builder/BuilderLunchOrderDemo.java
@@ -0,0 +1,17 @@
+package com.compi.builder;
+
+public class BuilderLunchOrderDemo {
+
+    public static void main(String[] args) {
+
+        LunchOrder.Builder builder = new LunchOrder.Builder();
+        builder.bread("wheat").condiments("Lettuce").dressing("Mustard").meat("Ham");
+
+        LunchOrder lunchOrder = builder.build();
+
+        System.out.println(lunchOrder.getBread());
+        System.out.println(lunchOrder.getCondiments());
+        System.out.println(lunchOrder.getDressing());
+        System.out.println(lunchOrder.getMeat());
+    }
+}
